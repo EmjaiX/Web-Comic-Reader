6cdb7cac01d282de6319eb6937b4c6867ad12877
Lexan E Flowers <lexar5657@gmail.com>
2022-07-31 08:32:38 -0600

    Create manga-data.sql

---
+++ assets/sql/manga-data.sql
@@ -0,0 +1,57 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.4.1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Jun 30, 2022 at 08:58 PM
+-- Server version: 5.7.11
+-- PHP Version: 5.6.18
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `manga_library`
+--
+
+--
+-- Dumping data for table `manga`
+--
+
+INSERT INTO `manga` (`id`, `name`, `chapter`, `filename`, `uploader`) VALUES
+(1, 'Dragon Ball Super', 'v06', 'Dragon Ball Super v06 (2019) (Digital) (1r0n).cbz', 'Zuschauer'),
+(2, 'Dragon Ball Super', '069', 'Dragon Ball Super 069 (2021) (Digital) (1r0n).cbz', 'Zuschauer'),
+(3, 'Dragon Ball Super', '070', 'Dragon Ball Super 070 (2021) (Digital) (1r0n).cbz', 'Zuschauer'),
+(4, 'Dragon Ball Super', '071', 'Dragon Ball Super 071 (2021) (Digital) (1r0n).cbz', 'Zuschauer'),
+(5, 'Dragon Ball Super', '072', 'Dragon Ball Super 072 (2021) (Digital) (1r0n).cbz', 'Zuschauer'),
+(6, 'Dragon Ball Super', '073', 'Dragon Ball Super 073 (2021) (Digital) (1r0n).cbz', 'Zuschauer'),
+(7, 'Dragon Ball Super', '074', 'Dragon Ball Super 074 (2021) (Digital) (1r0n).cbz', 'Zuschauer'),
+(8, 'Dragon Ball Super', '075', 'Dragon Ball Super 075 (2021) (Digital) (1r0n).cbz', 'Zuschauer'),
+(9, 'Dragon Ball Super', '076', 'Dragon Ball Super 076 (2021) (Digital) (1r0n).cbz', 'Zuschauer'),
+(10, 'Dragon Ball Super', '077', 'Dragon Ball Super 077 (2021) (Digital) (1r0n).cbz', 'Zuschauer'),
+(11, 'Dragon Ball Super', '078', 'Dragon Ball Super 078 (2021) (Digital) (1r0n).cbz', 'Zuschauer'),
+(12, 'Dragon Ball Super', '079', 'Dragon Ball Super 079 (2021) (Digital) (1r0n).cbz', 'Zuschauer'),
+(13, 'Dragon Ball Super', 'v01', 'Dragon Ball Super v01 (2017) (Digital) (1r0n).cbz', 'Zuschauer'),
+(14, 'Dragon Ball Super', 'v02', 'Dragon Ball Super v02 (2017) (Digital) (1r0n).cbz', 'Zuschauer'),
+(15, 'Dragon Ball Super', 'v03', 'Dragon Ball Super v03 (2018) (Digital) (1r0n).cbz', 'Zuschauer'),
+(16, 'Dragon Ball Super', 'v04', 'Dragon Ball Super v04 (2019) (Digital) (1r0n).cbz', 'Zuschauer'),
+(17, 'Dragon Ball Super', 'v05', 'Dragon Ball Super v05 (2019) (Digital) (1r0n).cbz', 'Zuschauer'),
+(18, 'Dragon Ball Super', 'v07', 'Dragon Ball Super v07 (2019) (Digital) (1r0n).cbz', 'Zuschauer'),
+(19, 'Dragon Ball Super', 'v08', 'Dragon Ball Super v08 (2020) (Digital) (1r0n).cbz', 'Zuschauer'),
+(20, 'Dragon Ball Super', 'v09', 'Dragon Ball Super v09 (2020) (Digital) (1r0n).cbz', 'Zuschauer'),
+(21, 'Dragon Ball Super', 'v10', 'Dragon Ball Super v10 (2020) (Digital) (1r0n).cbz', 'Zuschauer'),
+(22, 'Dragon Ball Super', 'v11', 'Dragon Ball Super v11 (2020) (Digital) (1r0n).cbz', 'Zuschauer'),
+(23, 'Dragon Ball Super', 'v12', 'Dragon Ball Super v12 (2021) (Digital) (1r0n).cbz', 'Zuschauer'),
+(24, 'Dragon Ball Super', 'v13', 'Dragon Ball Super v13 (2021) (Digital) (1r0n).cbz', 'Zuschauer'),
+(25, 'Dragon Ball Super', 'v14', 'Dragon Ball Super v14 (2021) (Digital) (1r0n).cbz', 'Zuschauer'),
+(26, 'Dragon Ball Super', 'v15', 'Dragon Ball Super v15 (2022) (Digital) (1r0n).cbz', 'Zuschauer');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
